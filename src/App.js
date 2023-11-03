c4bb381dd23adde590ddf85c15a19f6f0dd16249
= <=pdat9776@gmail.com>
2023-11-03 11:02:32 +0700

    Đạt làm trang hồ sơ

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import NoPage from "./Pages/NoPages/NoPages";
 import File from "./Pages/File/File";
+import ViewProfile from "./Pages/ViewProfile/ViewProfile";
+import ProfileList from "./Pages/ProfileList/ProfileList";
+import AddProfile from "./Pages/Add_Profile/Add_Profile";
 import Home from "./Pages/Home/Home";
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
     <Route index element={<Home />}/>
     <Route path="*" element={<NoPage />} />
     <Route path="/file" element={<File />}/>
+    <Route path="/view-profile" element={<ViewProfile />}/>
+    <Route path="/profile-list" element={<ProfileList />}/>
+    <Route path="/add-profile" element={<AddProfile />}/>
     </Route>
     </Routes>
     </BrowserRouter>
