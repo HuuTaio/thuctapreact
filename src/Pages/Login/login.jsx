f2fdd8f74ebc11058162dcd71b3955597c051ecc
huutai <taihuu0408@gmail.com>
2023-11-09 21:26:17 +0700

    login

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import "./login.css";
-
+import { Link } from "react-router-dom";
 const Login = () => {
     return (
         <div className='login'>
         
             <div>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
-  {/*---- Include the above in your HEAD tag --------*/}
   <div className="limiter">
     <div className="container-login100">
       <div className="wrap-login100">
@@ -20,20 +17,27 @@ const Login = () => {
             Đăng nhập thành viên
           </span>
           <div className="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+            {/* input ở đây */}
             <input className="input100" type="text" name="email" placeholder="Email" />
+           
             <span className="focus-input100" />
             <span className="symbol-input100">
               <i className="fa fa-envelope" aria-hidden="true" />
             </span>
+
           </div>
           <div className="wrap-input100 validate-input" data-validate="Password is required">
+            {/* input ở đây */}
+
             <input className="input100" type="password" name="pass" placeholder="Mật khẩu" />
-            <span className="focus-input100" />
+            
+            <span className="focus-input100" />      
             <span className="symbol-input100">
               <i className="fa fa-lock" aria-hidden="true" />
             </span>
           </div>
           <div className="container-login100-form-btn">
+            {/* input ở đây */}
             <button className="login100-form-btn">
               Đăng nhập
             </button>
@@ -42,15 +46,7 @@ const Login = () => {
             <span className="txt1">
               Quên &nbsp;
             </span>
-            <a className="txt2" href="#">
-               Email  / Mật khẩu?
-            </a>
-          </div>
-          <div className="text-center p-t-136">
-            <a className="txt2" href="#">
-              Tạo tài khoản của bạn
-              <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true" />
-            </a>
+            <Link> Email  / Mật khẩu?</Link>         
           </div>
         </form>
       </div>
